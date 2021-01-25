6243985467a9a757d52609b6c59b937091b4aef1
Bogdan Polak <bogdanpol@gmail.com>
2021-01-25 23:16:24 +0100

    Database EF Core Scaffolding

---
+++ Data.Cinema/Room.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace Data.Cinema
+{
+    public partial class Room
+    {
+        public Room()
+        {
+            Showtimes = new HashSet<Showtime>();
+        }
+
+        public int Roomid { get; set; }
+        public string Name { get; set; }
+        public int Rows { get; set; }
+        public int Columns { get; set; }
+
+        public virtual ICollection<Showtime> Showtimes { get; set; }
+    }
+}
---
+++ Data.Cinema/Showtime.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace Data.Cinema
+{
+    public partial class Showtime
+    {
+        public Showtime()
+        {
+            Tickets = new HashSet<Ticket>();
+        }
+
+        public string Showid { get; set; }
+        public int Movieid { get; set; }
+        public int Roomid { get; set; }
+        public DateTime Start { get; set; }
+
+        public virtual Movie Movie { get; set; }
+        public virtual Room Room { get; set; }
+        public virtual ICollection<Ticket> Tickets { get; set; }
+    }
+}
