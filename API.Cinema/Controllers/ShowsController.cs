9267bb0e4b7d359d5f17bde31d4ff02a747cee1b
Bogdan Polak <bogdanpol@gmail.com>
2021-01-26 23:55:46 +0100

    Added ShowsController

---
+++ API.Cinema/Controllers/ShowsController.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Data.Cinema;
+
+namespace API.Cinema.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ShowsController : ControllerBase
+    {
+        private readonly CinemaContext _context;
+
+        public ShowsController(CinemaContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Show
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Showtime>>> GetShowtimes()
+        {
+            return await _context.Showtimes.ToListAsync();
+        }
+
+        // GET: api/Show/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Showtime>> GetShowtime(string id)
+        {
+            var showtime = await _context.Showtimes.FindAsync(id);
+
+            if (showtime == null)
+            {
+                return NotFound();
+            }
+
+            return showtime;
+        }
+
+        // PUT: api/Show/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutShowtime(string id, Showtime showtime)
+        {
+            if (id != showtime.Showid)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(showtime).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ShowtimeExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Show
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPost]
+        public async Task<ActionResult<Showtime>> PostShowtime(Showtime showtime)
+        {
+            _context.Showtimes.Add(showtime);
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateException)
+            {
+                if (ShowtimeExists(showtime.Showid))
+                {
+                    return Conflict();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return CreatedAtAction("GetShowtime", new { id = showtime.Showid }, showtime);
+        }
+
+        // DELETE: api/Show/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Showtime>> DeleteShowtime(string id)
+        {
+            var showtime = await _context.Showtimes.FindAsync(id);
+            if (showtime == null)
+            {
+                return NotFound();
+            }
+
+            _context.Showtimes.Remove(showtime);
+            await _context.SaveChangesAsync();
+
+            return showtime;
+        }
+
+        private bool ShowtimeExists(string id)
+        {
+            return _context.Showtimes.Any(e => e.Showid == id);
+        }
+    }
+}
