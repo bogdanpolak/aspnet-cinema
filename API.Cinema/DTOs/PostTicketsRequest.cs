f867f448a4ee1c5d4c8cba5738f1ae598621a1d8
Bogdan Polak <bogdanpol@gmail.com>
2021-02-02 22:49:55 +0100

    Added Request DTO PostTicketsRequest

---
+++ API.Cinema/DTOs/PostTicketsRequest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace API.Cinema.DTOs
+{
+    public class PostTicketsRequest
+    {
+        public string ShowId { get; set; }
+        public int RowNum { get; set; }
+        public int SeatNum { get; set; }
+        public decimal Price { get; set; }
+
+        public PostTicketsRequest()
+        {
+        }
+
+        public override bool Equals(object obj)
+            => obj is PostTicketsRequest request &&
+                ShowId == request.ShowId &&
+                RowNum == request.RowNum &&
+                SeatNum == request.SeatNum &&
+                Price == request.Price;
+
+        public override int GetHashCode()
+            => HashCode.Combine(ShowId, RowNum, SeatNum, Price);
+    }
+}
