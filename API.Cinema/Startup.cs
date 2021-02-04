3fa78acaa37b6cf3cfc19ba3911feb7934e71ddc
Bogdan Polak <bogdanpol@gmail.com>
2021-02-04 23:28:20 +0100

    Added swagger with UI

--- API.Cinema/Startup.cs
+++ API.Cinema/Startup.cs
@@ -29,6 +29,9 @@ namespace API.Cinema
             services.AddControllers();
 
             services.AddDbContext<CinemaContext>();
+
+            // Register the Swagger generator, defining 1 or more Swagger documents
+            services.AddSwaggerGen();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -38,6 +41,16 @@ namespace API.Cinema
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            // Enable middleware to serve generated Swagger as a JSON endpoint.
+            app.UseSwagger();
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.),
+            // specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+                // swagger at server root: c.RoutePrefix = string.Empty;
+            });
 
             app.UseHttpsRedirection();
 
