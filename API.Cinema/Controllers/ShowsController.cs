e41dea71f92a960387e3ddbfc1da1b08bc56f5d4
Bogdan Polak <bogdanpol@gmail.com>
2021-02-10 00:50:46 +0100

    Cleaned ShowController

--- API.Cinema/Controllers/ShowsController.cs
+++ API.Cinema/Controllers/ShowsController.cs
@@ -1,13 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
+/* Cinema */
 using Data.Cinema;
 using API.Cinema.DTOs;
-using Data.Cinema.Entites;
 using Data.Cinema.Models;
 
 namespace API.Cinema.Controllers
