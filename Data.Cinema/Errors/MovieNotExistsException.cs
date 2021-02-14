7e27257960fb7ce22b1df64149d765ce68d9a7bf
Bogdan Polak <bogdanpol@gmail.com>
2021-02-14 18:19:26 +0100

    Moved exceptions to Data.Cinema.Errors

--- Data.Cinema/Errors/MovieNotExistsException.cs
+++ Data.Cinema/Errors/MovieNotExistsException.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using System.Runtime.Serialization;
 
-namespace Data.Cinema
+namespace Data.Cinema.Errors
 {
     [Serializable]
     internal class MovieNotExistsException : Exception
