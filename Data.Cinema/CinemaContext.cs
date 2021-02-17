c4b0c525be8dff4a855b7392c27edff614334499
Bogdan Polak <bogdanpol@gmail.com>
2021-02-17 21:09:09 +0100

    Switched to PostgreSQL

--- Data.Cinema/CinemaContext.cs
+++ Data.Cinema/CinemaContext.cs
@@ -34,12 +34,13 @@ namespace Data.Cinema
                 // see https://go.microsoft.com/fwlink/?linkid=2131148.
                 // For more guidance on storing connection strings,
                 // see http://go.microsoft.com/fwlink/?LinkId=723263.
-                optionsBuilder.UseInMemoryDatabase("CinemaDatabase");
+
                 /*
+                optionsBuilder.UseInMemoryDatabase("CinemaDatabase");
+                */
                 optionsBuilder
                     .UseLazyLoadingProxies()
                     .UseNpgsql("Server=localhost;Port=5432;Database=cinema;");
-                */
             }
         }
 
