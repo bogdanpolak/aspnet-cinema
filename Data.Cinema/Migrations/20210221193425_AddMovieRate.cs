291ee7c914ebf756fa39e581252a0c45c96b8a3e
Bogdan Polak <bogdanpol@gmail.com>
2021-02-24 17:14:21 +0100

    Fixed AddMovieRate migration (missing Rate)

--- Data.Cinema/Migrations/20210221193425_AddMovieRate.cs
+++ Data.Cinema/Migrations/20210221193425_AddMovieRate.cs
@@ -6,12 +6,19 @@ namespace Data.Cinema.Migrations
     {
         protected override void Up(MigrationBuilder migrationBuilder)
         {
-
+            migrationBuilder.AddColumn<int>(
+                name: "Rate",
+                table: "movies",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
         }
 
         protected override void Down(MigrationBuilder migrationBuilder)
         {
-
+            migrationBuilder.DropColumn(
+                name: "Rate",
+                table: "movies");
         }
     }
 }
