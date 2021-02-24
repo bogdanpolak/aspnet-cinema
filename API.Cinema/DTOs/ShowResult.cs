97a34127917b2f801e688f8b78ecc572ce795d24
Bogdan Polak <bogdanpol@gmail.com>
2021-02-24 13:40:44 +0100

    Updated Shows result DTO

--- API.Cinema/DTOs/ShowResult.cs
+++ API.Cinema/DTOs/ShowResult.cs
@@ -5,13 +5,23 @@ namespace API.Cinema.DTOs
     public class ShowResult
     {
         public int ShowId { get; set; }
-        public string Movie { get; set; }
-        public string Room { get; set; }
-        public int Rate { get; set; }
-        public DateTime LaunchDate { get; set; }
         public DateTime Start { get; set; }
-        public int Seats { get; set; }
-        public int Sold { get; set; }
         public decimal Total { get; set; }
+        public MovieDetails Movie { get; set; }
+        public RoomDetails Room { get; set; }
+
+        public class MovieDetails
+        {
+            public string Name { get; set; }
+            public int Rate { get; set; }
+            public DateTime Launch { get; set; }
+        }
+
+        public class RoomDetails
+        {
+            public string Name { get; set; }
+            public int Seats { get; set; }
+            public int Sold { get; set; }
+        }
     }
 }
